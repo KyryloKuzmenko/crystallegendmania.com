b30ad538de1f1a11d2b26d7d87d6d2c25c7ca7ca
Jango <kkuzmenko1992@gmail.com>
2025-07-16 11:04:29 -0230

    basic markup and hero, header finished

--- src/js/header.js
+++ src/js/header.js
@@ -1,23 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
   const menuBtn = document.querySelector('.burger-menu');
+  const closeMenu = document.querySelector('.close-menu');
   const backdrop = document.querySelector('.backdrop');
-  const headerContent = document.querySelector('.header-section .header-content');
   const links = document.querySelectorAll('.backdrop-list-item');
 
   menuBtn?.addEventListener('click', () => {
-    backdrop.classList.toggle('show');
-    headerContent.classList.toggle(backdrop.classList.contains('show'));
+    backdrop.classList.add('show');
+    menuBtn.classList.add('hidden');
+    closeMenu.classList.remove('hidden');
+  });
+
+  closeMenu?.addEventListener('click', () => {
+    backdrop.classList.remove('show');
+    menuBtn.classList.remove('hidden');
+    closeMenu.classList.add('hidden');
   });
 
   backdrop?.addEventListener('click', e => {
     if (e.target === backdrop) {
       backdrop.classList.remove('show');
+      menuBtn.classList.remove('hidden');
+      closeMenu.classList.add('hidden');
     }
   });
 
   links.forEach(link => {
     link.addEventListener('click', () => {
       backdrop.classList.remove('show');
+      menuBtn.classList.remove('hidden');
+      closeMenu.classList.add('hidden');
     });
   });
 });
